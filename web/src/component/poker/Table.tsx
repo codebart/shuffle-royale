f526fbc9eaa276a4c944d6eeacfc0b263cdb17e2
bstachowski <bartlomiej.j.stachowski@gmail.com>
2024-03-19 10:33:50 +0100

    Added table component layout

---
+++ web/src/component/poker/Table.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Table = () => (
+    <svg viewBox="0 0 96 54">
+        <path fill="#333333" d="m 0 27 q 0 -27 27 -27 l 42 0 q 27 0 27 27 q 0 27 -27 27 l -43 0 q -27 0 -26 -27 z"/>
+        <path fill="#0f427c" d="m 5 27 q 0 -22 22 -22 l 42 0 q 22 0 22 22 q 0 22 -22 22 l -43 0 q -21 0 -21 -22 z"/>
+        <path fill="none" strokeWidth={0.5} stroke="#082546" d="m 15 27 q 0 -12 12 -12 l 42 0 q 12 0 12 12 q 0 12 -12 12 l -43 0 q -11 0 -11 -12 z"/>
+        <path fill="none" strokeWidth={1} stroke="#eabb00" d="m 5 27 q 0 -22 22 -22 l 42 0 q 22 0 22 22 q 0 22 -22 22 l -43 0 q -21 0 -21 -22 z"/>
+    </svg>
+)
