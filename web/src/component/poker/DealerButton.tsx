1ffa244ec852423b76bfd8207ad0424cb151e172
bstachowski <bartlomiej.j.stachowski@gmail.com>
2024-03-21 11:26:05 +0100

    Added dealer button component

---
+++ web/src/component/poker/DealerButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const DealerButton = () => (
+    <svg viewBox="-16 -16 32 32" width={32} height={32}>
+        <path fill="#ff9600" d="M -16 0 A 1 1 0 0 0 16 0 A 1 1 0 0 0 -16 0 Z"/>
+        <path fill="#fffc00" d="M -16 0 C -12 18 13 18 16 0 A 1 1 0 0 0 -16 0 Z"/>
+        <path fill="#ffcc00" d="M -14 0 C -12 15 13 15 14 0 A 1 1 0 0 0 -14 0 Z"/>
+        <path fill="#000000" d="M -4 6 L -4 -10 L -2 -10 C 10 -10 11 6 -2 6 Z"/>
+        <path fill="#ffcc00" d="M -1 3 L -1 -7 C 6 -7 7 3 -1 3 Z"/>
+    </svg>
+)
