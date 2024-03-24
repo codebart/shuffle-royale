603368709b9695287c337afdd95b7eea4cbc054a
bstachowski <bartlomiej.j.stachowski@gmail.com>
2024-03-24 21:39:48 +0100

    Chips component refactor

--- web/src/component/poker/chip/Chips.tsx
+++ web/src/component/poker/chip/Chips.tsx
@@ -3,6 +3,15 @@ import styled from 'styled-components';
 import React from 'react';
 import {ChipsStack} from './ChipsStack';
 
+const useChipText = (value: number): string => {
+    if (value >= 1000000 && value % 100000 === 0) {
+        return `${value / 1000000}M`
+    } else if (value >= 1000 && value % 100 === 0) {
+        return `${value / 1000}K`;
+    }
+    return String(value);
+};
+
 const useChipStacks = (value: number): ChipCounts => {
     const chips: Partial<ChipCounts> = {};
     let chipCount = value;
@@ -16,15 +25,32 @@ const useChipStacks = (value: number): ChipCounts => {
 
 export const Chips = ({value}: { value: number }) => {
     const stacks = useChipStacks(value);
+    const text = useChipText(value);
     return (
         <ChipsContainer>
-            {chipValueTypesAsc.filter(key => stacks[key])
-                .map(key => <ChipsStack key={key} chips={stacks[key]} value={key}/>)}
+            <ChipStacksContainer>
+                {chipValueTypesAsc.filter(key => stacks[key])
+                    .map(key => <ChipsStack key={key} chips={stacks[key]} value={key}/>)}
+            </ChipStacksContainer>
+            <ChipsValue>
+                {text}
+            </ChipsValue>
         </ChipsContainer>
     );
 };
 
 const ChipsContainer = styled.div`
   display: flex;
+  flex-direction: column;
+`;
+
+const ChipStacksContainer = styled.div`
+  display: flex;
   gap: 2rem;
 `;
+
+const ChipsValue = styled.span`
+  text-shadow: 0 0 3px #000000;
+  font-weight: bold;
+  margin-left: 50%;
+`
