c2f61c69d8015c46e76368d243e3750016e88ee1
bstachowski <bartlomiej.j.stachowski@gmail.com>
2024-04-04 20:31:32 +0200

    Added CORS configuration

---
+++ server/infrastructure/security/src/main/java/com/shuffleroyale/security/cors/CorsSecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.shuffleroyale.security.cors;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@EnableWebSecurity
+@Configuration
+class CorsSecurityConfiguration {
+
+    @ConfigurationProperties(prefix = "spring.security.cors")
+    record CorsConfigurationProperties(List<String> origins) {
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
+        return http.cors(configurer -> configurer.configurationSource(corsConfigurationSource))
+                .build();
+    }
+
+    @Bean
+    CorsConfiguration corsConfiguration(CorsConfigurationProperties properties) {
+        var configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(properties.origins());
+        configuration.applyPermitDefaultValues();
+        return configuration;
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(CorsConfiguration corsConfiguration) {
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+
+}
