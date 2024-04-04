f7278e476ddabc074cc68cee6e34d212f4e2818c
bstachowski <bartlomiej.j.stachowski@gmail.com>
2024-04-04 21:25:16 +0200

    Added liquibase, jooq and database schema

--- server/application/src/main/java/com/shuffleroyale/Application.java
+++ server/application/src/main/java/com/shuffleroyale/Application.java
@@ -2,7 +2,9 @@ package com.shuffleroyale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 class Application {
 
